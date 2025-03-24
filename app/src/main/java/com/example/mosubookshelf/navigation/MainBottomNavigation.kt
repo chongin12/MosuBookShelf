8f364fb91f3434a24d1fd9cbbdafb3c64f2a98c2
mosu <mosu@channel.io>
2025-03-24 16:42:45 +0900

    fix: 하단 Navigation 탭 기억 & 탭 액션 정의

--- app/src/main/java/com/example/mosubookshelf/navigation/MainBottomNavigation.kt
+++ app/src/main/java/com/example/mosubookshelf/navigation/MainBottomNavigation.kt
@@ -5,8 +5,7 @@ import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
@@ -16,27 +15,30 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 
 @Composable
 fun MainBottomNavigation(navController: NavController) {
+    var selectedRoute by remember { mutableStateOf(topLevelRoutes.first().routeID) }
     BottomNavigation(
         backgroundColor = Color.White,
         contentColor = Color.Magenta,
         modifier = Modifier
             .navigationBarsPadding()
     ) {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = navBackStackEntry?.destination?.route
         topLevelRoutes.forEach { topLevelRouteType ->
             BottomNavigationItem(
                 icon = { Icon(topLevelRouteType.icon, contentDescription = topLevelRouteType.name) },
                 label = { Text(topLevelRouteType.navName) },
-                selected = currentRoute == topLevelRouteType.routeID,
+                selected = selectedRoute == topLevelRouteType.routeID,
                 onClick = {
-                    navController.navigate(topLevelRouteType.routeID) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
+                    if (selectedRoute == topLevelRouteType.routeID) {
+                        navController.popBackStack(topLevelRouteType.routeID, inclusive = false)
+                    } else {
+                        selectedRoute = topLevelRouteType.routeID
+                        navController.navigate(topLevelRouteType.routeID) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
                         }
-
-                        launchSingleTop = true
-                        restoreState = true
                     }
                 },
                 alwaysShowLabel = false,
